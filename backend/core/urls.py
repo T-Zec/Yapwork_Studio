4879d21d5ed5f469eb9243014baf93a805082804
t-zec <sagarsp8920@gmail.com>
2026-04-21 21:35:46 +0530

    Setup: Implement Cloudinary media support for static files

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -15,7 +15,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+from django.views.static import serve
 
 from django.conf import settings
 from django.conf.urls.static import static
@@ -37,6 +38,10 @@ urlpatterns = [
     # JWT
     path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+
+    re_path(r'^media/(?P<path>.*)$', serve, {
+        'document_root': settings.MEDIA_ROOT
+    }),
 ]
 
 if settings.DEBUG:
